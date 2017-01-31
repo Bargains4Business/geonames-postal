aeff5b9ddbc0f09fa626de32458f6b68514b5801
Kleber Correia <klebervirgilio@gmail.com>
2017-01-31 12:21:14 +1000

    compare downcased and striped strings

--- app/models/geo_names/geo_name.rb
+++ app/models/geo_names/geo_name.rb
@@ -1,7 +1,7 @@
 module GeoNames
   class GeoName < ActiveRecord::Base
     scope :ordered, -> { order(:place_name) }
-    scope :by_postal_code, ->(postal_code) { where("postal_code like ?", "#{postal_code}%").ordered }
-    scope :by_country, ->(country) { where("country_name = :country OR country_code = :country OR country_alpha3 = :country", country: country).ordered }
+    scope :by_postal_code, ->(postal_code) { where("lower(postal_code) like ?", "#{postal_code.downcase}%").ordered }
+    scope :by_country, ->(country) { where("lower(country_name) = :country OR lower(country_code) = :country OR lower(country_alpha3) = :country", country: country.downcase.strip).ordered }
   end
 end

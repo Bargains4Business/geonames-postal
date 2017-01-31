aa4a7a0697b6e0c68b552bc5302ce526bae29149
Kleber Correia <klebervirgilio@gmail.com>
2017-02-01 08:32:47 +1000

    Fix typo

--- app/controllers/geo_name_postals/codes_controller.rb
+++ app/controllers/geo_name_postals/codes_controller.rb
@@ -3,7 +3,7 @@ require_dependency "geo_name_postals/application_controller"
 module GeoNamePostals
   class CodesController < ApplicationController
     def search
-      return render json: { error: 'postal_code and country are requiredss'}, status: 422 if params[:postal_code].blank? || params[:country].blank?
+      return render json: { error: 'postal_code and country are required'}, status: 422 if params[:postal_code].blank? || params[:country].blank?
       @geo_name_postals = CodeQuery.new(params[:postal_code].strip, params[:country].strip).perform
     end
   end
